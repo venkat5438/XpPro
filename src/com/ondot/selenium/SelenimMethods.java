d4673c22cc165eced0fed8726052439f79c82323
venkat5438 <venkatramreddy108@gmail.com>
2017-02-12 14:08:10 +0530

    file hase been changed

--- src/com/ondot/selenium/SelenimMethods.java
+++ src/com/ondot/selenium/SelenimMethods.java
@@ -10,15 +10,19 @@ public class SelenimMethods {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","D:\\naa files\\selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver w=new ChromeDriver();
-		w.get("https://www.facebook.com/login/");
-		WebElement ele=w.findElement(By.name("email"));
-		ele.sendKeys("venkatramreddy108@gmail.com");
-		String str= ele.getAttribute("type");
-		System.out.println(str);
+		w.get("https://www.facebook.com/login.php?login_attempt=1&lwv=110");
+		w.findElement(By.name("email")).sendKeys("venkatramreddy108@gmail.com");
+		w.findElement(By.name("pass")).sendKeys("execute");
+		w.findElement(By.name("login")).click();
+		
+		//WebElement ele=w.findElement(By.name("email"));
+		//ele.sendKeys("venkatramreddy108@gmail.com");
+		//String str= ele.getAttribute("name");
+		//System.out.println(str);
 		//ele.submit();
-	    //w.wait(5000);
+	    w.wait(5000);
 	    //w.close();
-	    //w.quit();
+	    w.quit();
 
 	}
 
