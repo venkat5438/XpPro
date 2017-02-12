5aa221d13bed919fbe115481419eadf3358c3862
venkat5438 <venkatramreddy108@gmail.com>
2017-02-12 12:26:57 +0530

    commited

--- src/com/ondot/selenium/Selenium.java
+++ src/com/ondot/selenium/Selenium.java
@@ -5,16 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 public class Selenium {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","D:\\naa files\\selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver w=new ChromeDriver();
+		//WebDriver w=new FirefoxDriver();
 		w.get("https://google.com");
+		String Url= w.getCurrentUrl();
+		String Title= w.getTitle();
 		WebElement e=w.findElement(By.name("q"));
 		Thread.sleep(5000);
 		e.sendKeys("maheshbabu");
 		e.submit();
+		System.out.println(Url);
+		System.out.println(Title);
 		Thread.sleep(5000);
 		w.quit();
 
