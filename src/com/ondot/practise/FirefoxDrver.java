293ccdcfacedfbe137b83a4472280ccb6a53d0f0
venkat5438 <venkatramreddy108@gmail.com>
2017-02-13 11:44:47 +0530

    firefox class added

---
+++ src/com/ondot/practise/FirefoxDrver.java
@@ -0,0 +1,61 @@
+package com.ondot.practise;
+
+import com.ondot.common.WebDriver;
+
+public class FirefoxDrver implements WebDriver {
+
+	@Override
+	public void get() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void manage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCurrentUrl() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getTitle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void navigate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getWindowHandle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
